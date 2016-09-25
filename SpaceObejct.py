5cd54fe8619fb104fcdcdc3f581ef8763524ab61
chshibo <chshibo@umich.edu>
2016-09-25 16:39:30 -0400

    Added distance method in spaceobject

--- SpaceObejct.py
+++ SpaceObejct.py
@@ -1,6 +1,8 @@
 #unit m/s
 import math
 CONST_SPEED_OF_LIGHT=299792458
+#Hubble Constant from SDSS-III (2016-7-13) +0.7~-0.6
+HUBBLE_CONSTANT=67.6
 
 class SpaceObject(object):
     """Objects contained in our universe including blackholes, galaxies,star,planets etc."""
@@ -11,9 +13,10 @@ class SpaceObject(object):
         self.redshift=redshift
 
     #+going away ,-for moving closer
-
+   #unit:m/s
     def velocityOfObject(self):
         return ((math.pow(self.redshift,2)+2*self.redshift)/\
                 (math.pow(self.redshift,2)+2*self.redshift+2))*CONST_SPEED_OF_LIGHT
-
-
+    #unit:Mpc
+    def distance(self):
+        return self.velocityOfObject()/(1000*HUBBLE_CONSTANT)
