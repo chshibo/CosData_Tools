5e8e07a336f0e86b228ecccbef1603a720b06ff7
chshibo <chshibo@umich.edu>
2016-09-25 09:51:21 -0400

    update the velocity of spaceobject

--- Galaxy.py
+++ Galaxy.py
@@ -1,4 +1,13 @@
 import SpaceObejct
+
+
+
 class Galaxy(SpaceObejct):
     """basic data about a galaxy"""
-    def __init__(self,objid,ra,dec=None,r=None,l_to_sun=None,redshift=None):
+    def __init__(self,objid,ra,dec,r=None,l_to_sun=None,redshift=None):
+        SpaceObejct.__init__(self,objid,ra,dec,redshift)
+        self.r=r
+        self.l_to_sun=l_to_sun
+        self.redshift=redshift
+
+
--- SpaceObejct.py
+++ SpaceObejct.py
@@ -1,3 +1,7 @@
+#unit m/s
+import math
+CONST_SPEED_OF_LIGHT=299792458
+
 class SpaceObject(object):
     """Objects contained in our universe including blackholes, galaxies,star,planets etc."""
     def __init__(self,objid,ra,dec,redshift=None):
@@ -5,3 +9,9 @@ class SpaceObject(object):
         self.ra=ra
         self.dec=dec
         self.redshift=redshift
+
+    #+going away ,-for moving closer
+
+    def velocityOfObject(self):
+        return ((math.pow(self.redshift,2)+2*self.redshift)/\
+                (math.pow(self.redshift,2)+2*self.redshift+2))*CONST_SPEED_OF_LIGHT
